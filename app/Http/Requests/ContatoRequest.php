daecf3ce1180975d7e16e10903d09aa1b3422744
Joao <joaosantos.rodrigues9@gmail.com>
2019-03-27 14:06:01 -0400

    Helper e validação de formularios

--- app/Http/Requests/ContatoRequest.php
+++ app/Http/Requests/ContatoRequest.php
@@ -13,7 +13,7 @@ class ContatoRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,8 +23,46 @@ class ContatoRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            //
-        ];
+
+        switch ($this->method()) {
+
+            case 'POST'://criação de registro
+                return 
+                [
+                    'saudaçao'   =>   'requered|max:5',
+                    'nome'       =>   'requered|max:100',
+                    'telefone'   =>   'requered|max:15',
+                    'email'      =>   'email|max:200|unique:contato',
+                    'data_nascimento' =>   'date_format:"d/m/Y"',
+                    'avatar'     =>   'nullable|sometimes|image|mimes:jpg,jpeg,png,gif',
+                ];
+                break;
+            case 'PUT'://atualização de registro
+                return 
+                [
+                    'saudaçao'   =>   'requered|max:5',
+                    'nome'       =>   'requered|max:100',
+                    'telefone'   =>   'requered|max:15',
+                    'email'      =>   'email|max:200|unique:contato,email,'.$this->id,
+                    'data_nascimento' =>   'date_format:"d/m/Y"',
+                    'avatar'     =>   'nullable|sometimes|image|mimes:jpg,jpeg,png,gif',
+                ];
+                break;
+
+            default:break;
+        }
+        
     }
+    public function messages(){
+
+        return
+        [
+            'saudaçao.requered' => 'O campo saudaçao é Obrigatorio',
+            'nome.requered' => 'O campo nome é Obrigatorio',
+            'email.requered' => 'Informe um email valido',
+            'data_nascimento.date_format' => 'O campo data deve ser no formato DD/MM/AAAA'
+        ];
+        
+    }
+
 }
